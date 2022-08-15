658a5c345a8658643dc9f7cd703b102069aa2b09
Sandra <cheptoosandra.t@gmail.com>
2022-08-15 21:01:00 +0300

    Post Schema

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const UserSchema = new mongoose.schema({
     username:{
         type: String,
         required: true,
-        unique: true,
+        unique: true
     },
     email:{
         type: String,
