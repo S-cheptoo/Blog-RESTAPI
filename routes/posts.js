1f4bc020220148411282ab31f0f12245aa099ef3
Sandra <cheptoosandra.t@gmail.com>
2023-01-05 22:16:44 +0300

    Update Post

--- routes/posts.js
+++ routes/posts.js
@@ -4,7 +4,7 @@ const Post = require("../models/Post");
 const bcrypt = require("bcrypt");
 
 //CREATE POST
-router.put("/:id", async (req, res)=>{
+router.post("/:id", async (req, res)=>{
     const newPost = new Post(req.body);
     try{
         const savedPost = newPost.save();
@@ -16,23 +16,25 @@ router.put("/:id", async (req, res)=>{
 });
 
 //UPDATE
-router.delete("/:id", async (req, res)=>{
-    if(req.body.userId === req.params.id){
-        try{
-            const user =await User.findById(req.params.id);
-            try {
-                await Post.deleteMany({ username: user.username});
-                await User.findByIdAndDelete(req.params.id)
-                res.status(200).json("User has been deleted!");
-            }catch(err){
-                //respond with status 500 and return error
+router.put("/:id", async (req, res)=>{
+    try{
+        const post = await Post.findById(res.params.id);
+        if(post.username === req.body.username){
+            try{
+                const updatedPost = await Post.findByIdAndUpdate(req.params.id,{
+                    $set:req.body,
+                },
+                {new:true}
+            );
+            res.status(200).json(updatedPost);
+            }catch(err) {
                 res.status(500).json(err);
             }
-        } catch (err){
-            res.status(404).json("User not found!");
+        }else {
+            res.status(401).json("You can only update your post!")
         }
-    } else{
-        res.status(401).json("You can only delete your account!");
+    } catch (err){
+        res.status(500).json(err);
     }
 });
 
