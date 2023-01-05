65bf4f2abc6f5df1d2b98490df1943c0b5f82a66
Sandra <cheptoosandra.t@gmail.com>
2023-01-06 00:55:04 +0300

    Uploading an image file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
-const multer = require("multer")
+const multer = require("multer");
 
 dotenv.config();
 app.use(express.json());
@@ -18,6 +18,20 @@ mongoose.connect(process.env.MONGO_URL, {
 })
 .then(console.log("Connected to MongoDB"))
 .catch((err)=> console.log(err));
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb) => {
+        cb(null,"images");
+    },
+    filename:(req,file,cb) => {
+        cb(null,req.body.name);
+    },
+});
+
+const upload = multer({storage:storage});
+app.post("/api/upload", upload.single("file"),(req,res)=>{
+    res.status(200).json("File has been uploaded");
+})
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
